b8a84e6ef9d187efdbabe819b0b00f069ab6acbe
Artur Ardai <krkerdo@gmail.com>
2024-02-06 14:38:28 +0300

    test

--- View/AboutUs.php
+++ View/AboutUs.php
@@ -3,7 +3,7 @@
     $pageTitle = "";
 
     // Set the page content
-    $pageContent = 'Content/AboutUs-content.php';
+    $pageContent = 'AboutUs-content.php';
 
     // Include the template file
     include 'template.php';
