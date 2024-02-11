fdcaf97873c89be6847521e41ea091b14cfba843
Artur Ardai <krkerdo@gmail.com>
2024-02-11 19:02:57 +0300

    Change connection

--- API/Config/Database.php
+++ API/Config/Database.php
@@ -1,11 +1,11 @@
 <?php
 
 class Database{
-private $host='localhost';
-private $db_name='proiectweb';
-private $username='root';
-private $passwordEmpty='';
-private $conn;
+   private $host='localhost';
+   private $db_name='tibclau';
+   private $username='root';
+   private $password='';
+   private $conn;
 
 
 public function connect()
@@ -14,7 +14,7 @@ public function connect()
  try
  {
     $this->conn=new PDO('mysql:host='.$this->host.';dbname='.$this->db_name,
-    $this->username,$this->passwordEmpty);
+    $this->username,$this->password);
     $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
  }  
  catch(PDOException $e)
