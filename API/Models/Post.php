83d801a99093afa8a3c2c26bcccf5fa747d6f62a
Artur Ardai <krkerdo@gmail.com>
2024-02-17 18:08:58 +0300

    big fail now correcting it the post will have an img and i add img url to the table of it

--- API/Models/Post.php
+++ API/Models/Post.php
@@ -12,7 +12,7 @@ public $body;
 public $auth;
 public $created_at;
 public $expire;
-
+public $img_url;
 
 public function __construct($db)
 {
@@ -28,7 +28,8 @@ public function read()
     p.body,
     p.author,
     p.created_at,
-    p.expire
+    p.expire,
+    p.img_url
     FROM
     '.$this->table.' p
     LEFT JOIN
@@ -53,7 +54,8 @@ public function read_single()
     p.body,
     p.author,
     p.created_at,
-    p.expire
+    p.expire,
+    p.img_url
     FROM
     '.$this->table.' p
     LEFT JOIN
@@ -77,7 +79,7 @@ public function read_single()
     $this->category_id=$row['category_id'];
     $this->category_name=$row['category_name'];
     $this->expire=$row['expire'];
-
+    $this->img_url=$row['img_url'];
 
     
 }
@@ -90,7 +92,8 @@ public function create()
     body=:body,
     author=:auth,
     category_id=:category_id,
-    expire=:expire
+    expire=:expire,
+    img_url=:img_url
     ';
     $statment=$this->conn->prepare($query);
     $this->title=htmlspecialchars(strip_tags($this->title));
@@ -98,12 +101,14 @@ public function create()
     $this->auth=htmlspecialchars(strip_tags($this->auth));
     $this->category_id=htmlspecialchars(strip_tags($this->category_id));
     $this->expire=htmlspecialchars(strip_tags($this->expire));
+    $this->img_url=htmlspecialchars(strip_tags($this->img_url));
 
     $statment->bindParam(':title',$this->title);
     $statment->bindParam(':body',$this->body);
     $statment->bindParam(':auth',$this->auth);
     $statment->bindParam(':category_id',$this->category_id);
     $statment->bindParam(':expire',$this->expire);
+    $statment->bindParam(':img_url',$this->img_url);
     if($statment->execute())
     {
         return true;
@@ -124,7 +129,8 @@ public function update()
     body=:body,
     author=:auth,
     category_id=:category_id,
-    expire=:expire
+    expire=:expire,
+    img_url=:img_url
     WHERE
         id=:id
     ';
@@ -135,13 +141,15 @@ public function update()
     $this->category_id=htmlspecialchars(strip_tags($this->category_id));
     $this->id=htmlspecialchars(strip_tags($this->id));
     $this->expire=htmlspecialchars(strip_tags($this->expire));
+    $this->img_url=htmlspecialchars(strip_tags($this->img_url));
 
     $statment->bindParam(':title',$this->title);
     $statment->bindParam(':body',$this->body);
     $statment->bindParam(':auth',$this->auth);
     $statment->bindParam(':category_id',$this->category_id);
-    $statment->bindParam('id',$this->id);
+    $statment->bindParam(':id',$this->id);
     $statment->bindParam(':expire',$this->expire);
+    $statment->bindParam(':img_url',$this->img_url);
     if($statment->execute())
     {
         return true;
