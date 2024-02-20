8aca032be28794b725e9f6bff40a8d1356775aaa
Artur Ardai <krkerdo@gmail.com>
2024-02-20 14:36:48 +0300

    fix

--- autoload.php
+++ autoload.php
@@ -1,5 +1,10 @@
 <?php
 spl_autoload_register(function ($class_name) {
-    include $class_name . '.php';
+    // Convert class name to file path by replacing namespace separator with directory separator
+    $file_path = str_replace('\\', DIRECTORY_SEPARATOR, $class_name) . '.php';
+    // Check if the file exists and include it
+    if (file_exists($file_path)) {
+        include $file_path;
+    }
 });
 ?>
